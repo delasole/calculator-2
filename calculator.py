8773986942816817c678569c799d2e4b2bc161c5
Luciana <luciana.cterrazas@gmail.com>
2020-07-19 02:09:12 +0000

    Created loop function for calculator

--- calculator.py
+++ calculator.py
@@ -6,35 +6,39 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 # Replace this with your code
 
-user_input = input("Enter your equation >> ")
-output = ""
-
-def process(user_input):
-    while True:
-        token = user_input.split(' ')
-        if(token[0]== 'q'):
+while True:
+    user_input = input("Enter your equation >> ")
+    output = ""
+    token = user_input.split(' ')
+    if(token[0]== 'q'):
+        print("You have left")
+        break
+    else:
+        if(token[0] == '+'):
+            output = add(float(token[1]),float(token[2]))
+            print(output)
+        if(token[0] == '-'):
+            output = subtract(float(token[1]),float(token[2]))
+            print(output)
+        if(token[0] == '*'):
+            output = multiply(float(token[1]),float(token[2]))
+            print(output)
+        if(token[0] == '/'):
+            output = divide(token[1],float(token[2]))
+            print(output)
+        if(token[0] == 'square'):
+            output = square(float(token[1]))
+            print(output)
+        if(token[0] == 'cube'):
+            output = cube(float(token[1]))
+            print(output)
+        if(token[0] == 'pow'):
+            output = power(float(token[1]),float(token[2]))
+            print(output)
+        if(token[0] == 'mod'):
+            output = mod(float(token[1]),float(token[2]))
+            print(output)
+        if(token[0] == 'quit'):
             print("You have left")
             break
-        else:
-            if(token[0] == '+'):
-                output = add(float(token[1]),float(token[2]))
-            if(token[0] == '-'):
-                output = subtract(float(token[1]),float(token[2]))
-            if(token[0] == '*'):
-                output = multiply(float(token[1]),float(token[2]))
-            if(token[0] == '/'):
-                output = divide(token[1],float(token[2]))
-            if(token[0] == 'square'):
-                output = square(float(token[1]))
-            if(token[0] == 'cube'):
-                output = cube(float(token[1]))
-            if(token[0] == 'pow'):
-                output = power(float(token[1]),float(token[2]))
-            if(token[0] == 'mod'):
-                output = mod(float(token[1]),float(token[2]))
-            if(token[0] == 'quit'):
-                print("You have left")
-                break
-        return output
 
-print(process(user_input))
