21d23e4c09f1970a0d8d2af49b043f4dda0adb21
Luciana <luciana.cterrazas@gmail.com>
2020-07-19 00:46:44 +0000

    Formatted if statements to execute properly

--- calculator.py
+++ calculator.py
@@ -12,10 +12,27 @@ output = ""
 print(f"You entered {user_input}")
 
 def process(user_input):
-    while user_input != "q":
+    while user_input != " ":
         token = user_input.split(' ')
-        if(token[0] == '+'):
-            output = add(float(token[1]),float(token[2]))
+        if(token[0]== 'q'):
+            break
+        else:
+            if(token[0] == '+'):
+                output = add(float(token[1]),float(token[2]))
+            if(token[0] == '-'):
+                output = subtract(float(token[1]),float(token[2]))
+            if(token[0] == '*'):
+                output = multiply(float(token[1]),float(token[2]))
+            if(token[0] == '/'):
+                output = divide(token[1],float(token[2]))
+            if(token[0] == 'square'):
+                output = square(float(token[1]))
+            if(token[0] == 'cube'):
+                output = cube(float(token[1]))
+            if(token[0] == 'pow'):
+                output = power(float(token[1]),float(token[2]))
+            if(token[0] == 'mod'):
+                output = mod(float(token[1]),float(token[2]))
         return output
 
 print(process(user_input))
